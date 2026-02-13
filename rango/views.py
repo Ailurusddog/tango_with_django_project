d0bff7e455fbf234c6c5c6cae788e0e3d27bf8fb
Ailurusddog <160128615+Ailurusddog@users.noreply.github.com>
2026-02-13 09:57:30 +0000

    Finish Chapter 4 templates, static and media

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
